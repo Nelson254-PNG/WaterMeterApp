89c25028e179897e90bc567c5d00c36f6e26ab47
Nelson <in160000822@kisiiuniversity.ac.ke>
2026-06-25 14:13:36 +0300

    hosted the app

--- api/client.ts
+++ api/client.ts
@@ -1,4 +1,4 @@
-const BASE_URL = "http://192.168.137.1:8090";
+const BASE_URL = "https://unsidereal-justine-ovational.ngrok-free.dev";                        
 //API REQUEST
 async function apiRequest<T>(
   path: string,
